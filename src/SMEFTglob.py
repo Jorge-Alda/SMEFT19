ecd9fd6220f8f61bd9173d02887af22c7ed04a93
Jorge-Alda <jalda@unizar.es>
2019-05-17 17:34:37 +0200

    Fixed the code to plot 2-D fits

--- src/SMEFTglob.py
+++ src/SMEFTglob.py
@@ -7,28 +7,41 @@ import numpy as np
 import smelli
 from math import isinf
 import warnings
+import yaml
 
 '''
-WARNING: To use the latest experimental values of RK(*), you must work with the developement (github) version of flavio and modify some files of smelli
+WARNING: To use the latest experimental values of RK(*), you must work with version 1.4 of flavio and modify some files of smelli and flavio
 '''
 
 fits = ['likelihood_lfu_fcnc.yaml', 'likelihood_rd_rds.yaml', 'likelihood_ewpt.yaml', 'global']
 labels = {'likelihood_lfu_fcnc.yaml':r'$R_{K^{(*)}}$', 'likelihood_rd_rds.yaml':r'$R_{D^{(*)}}$', 'likelihood_ewpt.yaml': 'EW precission', 'global':'Global'}
 
-gl = smelli.GlobalLikelihood()
+
+gl = smelli.GlobalLikelihood()		
 
 cache = dict()
-plotting = False
-point = 0
 def update_cache(x, wfun):
 	if x not in cache.keys():
 		cache[x] = dict()
-		with warnings.catch_warnings():
+		with warnings.catch_warnings():	
 			warnings.simplefilter('ignore')
 			glpp = gl.parameter_point(wfun(x))
 			gldict = glpp.log_likelihood_dict()
 			for f in fits:
-				cache[x][f] = gldict[f]
+				g = gldict[f]
+				if isinf(g):
+					if f == 'global':
+						g = 0
+						for f2 in fits[:-1]:
+							g += cache[x][f2]
+					else: 
+						g = -70
+				cache[x][f] = g
+				
+
+def clearcache():
+	global cache
+	cache = dict()
 
 def likelihood_fit_cached(x, wfun, f):
 	xt = tuple(x)
@@ -41,7 +54,7 @@ def likelihood_global(x, wfun):
 		glpp = gl.parameter_point(wfun(x))
 		return glpp.log_likelihood_global()
 
-def plot(wfun, xmin, xmax, ymin, ymax):
+def plot(wfun, xmin, xmax, ymin, ymax, axlabels, fout):
 	import texfig # https://github.com/knly/texfig
 	fig=plt.figure(figsize=(4,4))
 	plt.xlim([xmin,xmax])
@@ -52,10 +65,10 @@ def plot(wfun, xmin, xmax, ymin, ymax):
 		print('Plotting ' + f) 
 		with warnings.catch_warnings():
 			warnings.simplefilter('ignore')
-			loglike = lambda x: likelihood_fit(x, wfun, f)
+			loglike = lambda x: likelihood_fit_cached(x, wfun, f)
 			flavio.plots.likelihood_contour(loglike , 1.1*xmin, 1.1*xmax, 1.1*ymin, 1.1*ymax, col=i, label=labels[f], interpolation_factor=5, n_sigma=(1,2))
 		i+=1
-	plt.xlabel(r'$C_{\ell q(1)}^{\mu} = C_{\ell q(3)}^{\mu}$')
-	plt.ylabel(r'$C_{\ell q(1)}^{\tau} = C_{\ell q(3)}^{\tau}$')
+	plt.xlabel(axlabels[0])
+	plt.ylabel(axlabels[1])
 	plt.legend(loc=4)
-	texfig.savefig('SMEFT_C13_mutau')
+	texfig.savefig(fout)
