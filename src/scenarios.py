a5f39b5181783ae3300ec0349079f7b0e678d4df
Jorge-Alda <jalda@unizar.es>
2019-06-04 17:53:04 +0200

    First results to the rotation parameters

--- src/scenarios.py
+++ src/scenarios.py
@@ -29,7 +29,7 @@ def scIX(x):
 	return Wilson({'lq1_1133': x[0]*1e-6, 'lq3_1133': x[0]*1e-6, 'lq1_2233': -x[0]*1e-6, 'lq3_2233': -x[0]*1e-6, 'lq1_3333': x[0]*1e-6, 'lq3_3333': x[0]*1e-6}, eft='SMEFT', basis='Warsaw', scale=1e3)
 
 def idemp(a,b):
-	return np.matrix([[a**2, a*b, a], [a*b, b**2, b], [a, b, 1]])/(1+ a**2 + b**2)
+	return np.matrix([[abs(a)**2, a*np.conj(b), a], [np.conj(a)*b, abs(b)**2, b], [np.conj(a), np.conj(b), 1]])/(1+ abs(a)**2 + abs(b)**2)
 
 def matrixwc(num, C, ll, lq):
 	wc = dict()  
@@ -60,23 +60,30 @@ def massrotation(x):
 	w3 = matrixwc('3', C3, ll, lq)
 	return Wilson({**w1,**w3}, eft='SMEFT', basis='Warsaw', scale=1e3)
 
-def scbI(x):
-	a = x[0]/x[2]
-	b = x[1]/x[2]
-	C = x[2]*(1+a**2+b**2)
-	return massrotation([C, C, a, b, 0, 0])
+def rotBI(x):
+	#x = [C, beta_l, beta_q])
+	return massrotation([x[0], x[0], 0, x[1], 0, x[2]])
 
-def scbII(x):
-	# x = [C1333, C2333, C3313, C3323, C3333]
-	al = x[0]/x[4]
-	bl = x[1]/x[4]
-	aq = x[2]/x[4]
-	bq = x[3]/x[4]
-	C = x[4]*(1+al**2+bl**2)*(1+aq**2+bq**2)
-	return massrotation([C, C, al, bl, aq, bq])
+def rotBII(x):
+	#x = [C, alpha_l, beta_l, alpha_q, beta_q])
+	return massrotation([x[0], x[0], x[1], x[2], x[3], x[4]])
 
-def Feruglio(x):
-	bl = x[0]/x[2]
-	bq = x[1]/x[2]
-	C = x[2]*(1+bl**2)*(1+bq**2)
-	return massrotation([C, C, 0, bl, 0, bq])
+def rot2lqU1(x, M=1.5):
+	if len(x)==3:
+		al = 0
+		aq = 0
+		bl = x[1]
+		bq = x[2]
+	elif len(x)==5:
+		al = x[1]
+		bl = x[2]
+		aq = x[3]
+		bq = x[4]
+	C = x[0]
+	ll = idemp(al, bl)
+	lq = idemp(aq, bq)
+	xL = np.matrix(np.zeros([3,3]))
+	for i in range(0,3):
+		for j in range(0,3):
+			xL[i,j] = np.sqrt(-2*M**2*C * ll[i,i] * lq[j,j])*np.exp(1j*np.angle(lq[j,2])-1j*np.angle(ll[i,2]))
+	return xL
