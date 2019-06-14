ccad388432d17c65ee0627235912379f325fc80a
Jorge-Alda <jalda@unizar.es>
2019-06-14 09:48:22 +0200

    Python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+setup(
+	name='SMEFT19',
+	version='0.1',
+	description='Global likelihood study with the SMEFT operators Olq1 and Qlq3',
+	license='MIT',
+	author='Jorge Alda',
+	author_email='jalda@unizar.es',
+	url='https://github.com/Jorge-Alda/SMEFT19',
+	packages=['SMEFT19'],
+)
