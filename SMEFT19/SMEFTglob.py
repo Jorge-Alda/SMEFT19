6eee9e7b6e54791dfc538a645f590e26d434c768
Jorge-Alda <jalda@unizar.es>
2021-11-16 09:49:32 +0100

    Updated list of observables

--- SMEFT19/SMEFTglob.py
+++ SMEFT19/SMEFTglob.py
@@ -84,12 +84,12 @@ Interfaces `flavio` to compute the NP prediction of a given observable.
 	if fastmeas(obs):
 		lh = gl.fast_likelihoods[lhname]
 		ml = lh.likelihood.measurement_likelihood
-		pred = ml.get_predictions_par(gl.par_dict, wc)
+		pred = ml.get_predictions_par(gl.par_dict_default, wc)
 		return pred[obs]
 	else:
 		lh = gl.likelihoods[lhname]
 		ml = lh.measurement_likelihood
-		pred = ml.get_predictions_par(gl.par_dict, wc)
+		pred = ml.get_predictions_par(gl.par_dict_default, wc)
 		return pred[obs]
 
 def pull_obs(x, obs, wfun):
@@ -118,20 +118,37 @@ Calculates the pull, in sigmas, of the prediction of a given observable in NP wi
 	return pull(-2*(ll-ll_central), 1)
 
 
-def loadobslist():
+def newlist():
+    '''
+Creates a `.yaml` file with a list of all observables available, ordered by their pull in the SM.    
+    '''
+	glSM = gl.parameter_point({}, scale=1000)
+	obsSM = glSM.obstable()
+	obscoll = list(obsSM['pull exp.'].keys())
+	for i in range(len(obscoll)):
+		if isinstance(obscoll[i], tuple):
+			obscoll[i] = list(obscoll[i])
+	with open(__path__[0] + '/observables.yaml', 'wt') as fyaml:
+		yaml.dump(obscoll, fyaml)  
+
+def loadobslist(new=False):
 	'''
 Loads from a `.yaml` file a list of all observables available, ordered by their pull in the SM. If the file does not exist, this functions creates it.
 
 :Returns:
 	- A list with all observables available.
 	'''
-	try:
-		with open('observables.yaml', 'rt') as fyaml:
-			obscoll = yaml.safe_load(fyaml)
-	except:
-		glSM = gl.parameter_point({}, scale=1000)
-		obsSM = glSM.obstable()
-		obscoll = list(obsSM['pull exp.'].keys())
-		with open('observables.yaml', 'wt') as fyaml:
-			yaml.dump(obscoll, fyaml)
+	if new:
+		newlist()
+	else:	    
+		try:
+			with open(__path__[0] + '/observables.yaml', 'rt') as fyaml:
+				obscoll = yaml.safe_load(fyaml)
+		except (OSError, IOError):
+			newlist()
+	with open( __path__[0] + '/observables.yaml', 'rt') as fyaml:
+		obscoll = yaml.safe_load(fyaml)
+		for i in range(len(obscoll)):
+			if isinstance(obscoll[i], list):
+				obscoll[i] = tuple(obscoll[i])
 	return obscoll
